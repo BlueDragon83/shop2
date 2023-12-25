748f52540dbe8e02661ac77a19ddd58e0afc9ad2
blueDragon83 <jhr1843@gmail.com>
2023-12-26 07:33:03 +0900

    shop

---
+++ src/main/java/com/shop/dto/ItemDto.java
@@ -0,0 +1,28 @@
+package com.shop.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@ToString
+public class ItemDto {
+
+    private Long id;
+
+    private String itemNm;
+
+    private Integer price;
+
+    private String itemDetail;
+
+    private String sellStatCd;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+
+}
